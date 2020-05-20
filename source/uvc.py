c3ffe93e4536dcea18e3c0f28eb583500a4d93ae
rene <rwzeto@gmail.com>
2020-05-19 20:11:35 -0700

    eod

--- source/uvc.py
+++ source/uvc.py
@@ -4,14 +4,18 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # to do
-# gui plotting
-# physical parameters
-# - threshold
-# power delivered to surface
+# wavelength distributions
+# plane position slider
+# spatial distributions that match data sheets
 
 ZERO_THRESH = (1E-3)  # mm
-SQ_ZERO_THRESH = (1E-1)**2 # mm^2
+SQ_ZERO_THRESH = (1E-1)**2  # mm^2
+SURFACE_VALUE = -1E6
 
+GRID_MAX = 1 # mm
+GRID_RESOLUTION = .05 # mm
+SURFACE_DISTANCE = 1.8 # mm
+TOTAL_POWER = 10 # mW
 
 class world_grid:
     def __init__(self, grid_shape, grid_resolution):
@@ -93,27 +97,43 @@ class surface(world_data):
                 for z in self.world_grid.axis[2]:
                     if np.abs(A*(x-x0) + B*(y-y0) + C*(z-z0)) < ZERO_THRESH:
                         nx, ny, nz = self.world_grid.iloc((x, y, z))
-                        surface[nx][ny][nz] = 1
+                        surface[nx][ny][nz] = SURFACE_VALUE
         return surface
+    
+    def calculate_flux(self, intensity_distribution):
+        flux = 0
+        for x in self.world_grid.axis[0]: 
+            for y in self.world_grid.axis[1]:
+                for z in self.world_grid.axis[2]:
+                    nx, ny, nz = self.world_grid.iloc((x, y, z))
+                    if self.data[nx][ny][nz] != 0:
+                        flux += intensity_distribution.data[nx][ny][nz]*GRID_RESOLUTION**3
+        return flux
 
 class plot_gui:
     def __init__(self, intensity_distribution, plane):
         self.intensity_distribution = intensity_distribution
         self.plane = plane
-        plot_array = self.intensity_distribution.get_cross_section(x=0)+self.plane.get_cross_section(x=0)
+        plot_array = self.intensity_distribution.get_cross_section(x=-1)+self.plane.get_cross_section(x=-1)
 
+        # cube for now
+        extent_list = [-self.plane.world_grid.extent[0], self.plane.world_grid.extent[1], -self.plane.world_grid.extent[1], self.plane.world_grid.extent[1]]
         self.fig, self.ax = plt.subplots()
-        self.im = self.ax.imshow(plot_array, origin='lower')
-        self.im.set_clim(0, 5.0)
+        self.im = self.ax.imshow(plot_array, origin='lower', extent=extent_list, cmap='plasma')
+        self.im.set_clim(0, TOTAL_POWER*1.5)
+        self.ax.set_xlabel("Y position")
+        self.ax.set_ylabel("Z position")
 
         divider = make_axes_locatable(self.ax)
         cax = divider.append_axes("right", size="5%", pad=0.05)
-        plt.colorbar(self.im, ax=self.ax, cax=cax, label="Amplitude")
+        plt.colorbar(self.im, ax=self.ax, cax=cax, label="Intensity (mW/mm^2")
         plt.subplots_adjust(left=0.25, bottom=0.25)
 
         axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
-        sfreq = Slider(axfreq, 'x', -.9, .9, valinit=-.9, valstep=.1)
+        sfreq = Slider(axfreq, 'X position', -1, 1, valinit=-1, valstep=GRID_RESOLUTION)
         sfreq.on_changed(self.update_x)
+
+        self.ax.set_title("Incident surface power = %.2f mW\n %d mm^2 surface, LED power=%.2f mW"%(self.plane.calculate_flux(self.intensity_distribution), GRID_MAX, TOTAL_POWER))
 
         plt.show()
 
@@ -121,20 +141,11 @@ class plot_gui:
         self.im.set_data(self.intensity_distribution.get_cross_section(x=val)+self.plane.get_cross_section(x=val))
         self.fig.canvas.draw_idle()
 
-    def update_y(self, val):
-        self.im.set_data(self.intensity_distribution.get_cross_section(y=val))
-        self.fig.canvas.draw_idle()
 
-    def update_z(self, val):
-        self.im.set_data(self.intensity_distribution.get_cross_section(z=val))
-        self.fig.canvas.draw_idle()
+# cube for now
+world = world_grid((GRID_MAX, GRID_MAX, GRID_MAX), GRID_RESOLUTION)
 
-grid_shape = (1, 1, 1)  # mm
-grid_resolution = .05  # mm
-
-world = world_grid(grid_shape, grid_resolution)
-
-point_source = spherical(world, origin=(0, 0, 0), normal=np.array([0, 0, 0, ]), total_power=5.0)
-detector_plane = surface(world, origin=(0, .5, .5), normal=np.array([0, 1, 1])/np.sqrt(2))
+point_source = spherical(world, origin=(0, GRID_MAX, 0), normal=np.array([0, 0, 0, ]), total_power=TOTAL_POWER)
+detector_plane = surface(world, origin=(0, GRID_MAX-SURFACE_DISTANCE, 0), normal=np.array([0, 1, 0])/np.sqrt(2))
 
 plot_it = plot_gui(point_source, detector_plane)
